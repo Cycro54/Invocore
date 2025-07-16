b901ffc06a4382d180930817e1bf2ea457e8d7e7
Cycro54 <creatordebator@gmail.com>
2025-07-15 18:27:18 -0700

    1.21.1-5.2.4
    Fixes
    -Keybinds sent to this mod should now be properly registered.

--- src/main/java/invoker54/invocore/client/keybind/CustomKeybind.java
+++ src/main/java/invoker54/invocore/client/keybind/CustomKeybind.java
@@ -1,6 +1,7 @@
 package invoker54.invocore.client.keybind;
 
 import invoker54.invocore.Invocore;
+import invoker54.invocore.common.ModLogger;
 import net.minecraft.client.KeyMapping;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -9,31 +10,35 @@ import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @EventBusSubscriber(value = Dist.CLIENT, modid = Invocore.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class CustomKeybind {
+    public static final ModLogger LOGGERT = ModLogger.getLogger(CustomKeybind.class, new AtomicBoolean(true));
     public KeyMapping keyBind;
     public IClicked iClicked;
+    public static RegisterKeyMappingsEvent keyMappingEvent;
 
     public static final List<KeyMapping> KEY_MAPPING_LIST = new ArrayList<>();
 
     @SubscribeEvent
     public static void registerKeys(RegisterKeyMappingsEvent event){
+        keyMappingEvent = event;
+        if (KEY_MAPPING_LIST.isEmpty()) return;
         for (KeyMapping key : KEY_MAPPING_LIST){
-            event.register(key);
+            keyMappingEvent.register(key);
         }
     }
 
     public CustomKeybind(String name, int key, String MOD_ID, IClicked iClicked){
-        keyBind = new KeyMapping("key." + MOD_ID + "." + name, key,"key.category." + MOD_ID);
-        //keyBind = new KeyBinding(name, key,"XP Shop");
-        KEY_MAPPING_LIST.add(keyBind);
-        this.iClicked = iClicked;
+        this(new KeyMapping("key." + MOD_ID + "." + name, key,"key.category." + MOD_ID), iClicked);
     }
     public CustomKeybind(KeyMapping keyBind, IClicked iClicked){
         this.keyBind = keyBind;
-
         this.iClicked = iClicked;
+        KEY_MAPPING_LIST.add(keyBind);
+        if (keyMappingEvent == null) return;
+        keyMappingEvent.register(keyBind);
     }
 
     public void pressed(int action){
